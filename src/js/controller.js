b2591e9d761422cd28a88271c665d525c27be809
Hlumak <vglumakov@gmail.com>
2024-08-12 23:37:56 +0300

    Recipe rendering and MVC Architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,31 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const timeout = function(s) {
+  return new Promise(function(_, reject) {
+    setTimeout(function() {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlRecipes = async () => {
+  try {
+    const id = window.location.hash.slice(1);
 
-///////////////////////////////////////
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
