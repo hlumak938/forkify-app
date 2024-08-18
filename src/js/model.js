3f63f3cf1e09206c666edefc7859886d2bc15ae7
Hlumak <vglumakov@gmail.com>
2024-08-19 01:52:14 +0300

    Implemented bookmarks storaging

--- src/js/model.js
+++ src/js/model.js
@@ -70,13 +70,25 @@ export const updateServings = (newServings) => {
   state.recipe.servings = newServings;
 };
 
+const persistBookmarks = () => {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = (recipe) => {
   state.bookmarks.push(recipe);
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+  persistBookmarks();
 };
 
 export const deleteBookmark = (id) => {
   const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
   state.bookmarks.splice(index, 1);
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+  persistBookmarks();
 };
+
+const init = () => {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
